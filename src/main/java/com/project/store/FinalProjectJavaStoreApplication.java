ab9eae4c009274912d930057d1ea6c9adcb86740
Gomez Martinez <sagomezmartinez@deloitte.com>
2023-09-20 07:39:49 -0600

    Initial Commit

---
+++ src/main/java/com/project/store/FinalProjectJavaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectJavaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectJavaStoreApplication.class, args);
+	}
+
+}
