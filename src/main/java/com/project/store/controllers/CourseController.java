c5a17fabc569ab14f5aa9bd64a04e463a362a74e
sagomezmartinez <sagomezmartinez@USQROSAGOMEZMA1.us.deloitte.com>
2023-09-27 16:55:53 -0500

    Thymeleaf - First Commit

--- src/main/java/com/project/store/controllers/CourseController.java
+++ src/main/java/com/project/store/controllers/CourseController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +22,7 @@ import com.project.store.model.Course;
 import com.project.store.model.wrappers.CoursesWrapper;
 import com.project.store.service.CourseService;
 
+//@Controller
 @RestController
 @RequestMapping("courses")
 public class CourseController {
@@ -28,9 +33,14 @@ public class CourseController {
 		this.courseService = courseService;
 	}
 	
+//	@ModelAttribute("courseList")
 	@GetMapping
-	public List<Course> getAllCourses() {
-		return courseService.getAllCourses();
+//	public List<Course> getAllCourses() {
+//		return courseService.getAllCourses();
+//	}
+	public String getAllCourses(Model model) {
+		model.addAttribute("courseList", courseService.getAllCourses());
+	    return "courses";
 	}
 	
 	@GetMapping("/{id}")
@@ -50,6 +60,12 @@ public class CourseController {
 	public Course addCourse(@RequestBody Course course) {
 		return courseService.saveCourse(course);
 	}
+//	public String addCourse(@RequestBody Course course, BindingResult result, Model model) {
+//		if (result.hasErrors()) {
+//            return "add-user";
+//        }
+//		return "courses";
+//	}
 	
 	@PutMapping
 	public ResponseEntity<String> modifyCourse(@RequestBody Course newCourse) {
--- src/main/java/com/project/store/model/Course.java
+++ src/main/java/com/project/store/model/Course.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "courses")
+//@Table(name = "courses")
 public class Course {
 	
 	@Id
